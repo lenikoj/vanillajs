ae47a30f65f366f785e4ef448596d373701bb408
lena <ekolimitra@gmail.com>
2022-06-19 11:59:20 +0300

    Setup project

---
+++ main.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+function job(x, y) {
+  return x + y;
+}
+
+console.log("Hello world. This is a javascript script");
+
+let a = 6;
+let b = 10;
+
+let result = job(a, b);
+
+console.log("Result: ", result);
